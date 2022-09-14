307b60193859baef5da90e3ea63bc4bba4fdd7e4
ruchika110696 <ruchikal@netapp.com>
2022-09-14 15:53:05 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    netapp-cloudmanager = {
+      source  = "NetApp/netapp-cloudmanager"
+      version = ">= 22.8.2"
+    }
+  }
+}
+provider "netapp-cloudmanager" {
+  refresh_token = var.cloudmanager_refresh_token
+}
